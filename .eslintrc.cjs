1582ff8b57d6513d95c4edf37301db99f99a3edf
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-12-08 20:59:27 +0300

    chore: configure prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
