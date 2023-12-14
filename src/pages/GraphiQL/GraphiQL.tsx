ff74ef89d207c9b670345457f9e07d32084d3599
Novik Dmytro <dinovik@rambler.ru>
2023-12-14 17:49:45 +0300

    feat: add code editors to graphiQL page

--- src/pages/GraphiQL/GraphiQL.tsx
+++ src/pages/GraphiQL/GraphiQL.tsx
@@ -1,7 +1,38 @@
 import { FC } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import './codemirror.css';
+import { Button } from '@material-tailwind/react';
 
 const GraphiQL: FC = () => {
-  return <>GraphiQL Page</>;
+  return (
+    <div className="flex flex-col justify-center justify-items-center w-full h-full">
+      <div className="flex gap-1 w-full px-2 items-center">
+        <Button size="sm" variant="outlined">
+          Send
+        </Button>
+        <span>Endpoint: https://countries.trevorblades.com/</span>
+        <Button size="sm" variant="outlined" className="ml-auto">
+          Docs
+        </Button>
+      </div>
+      <div className="flex justify-center justify-items-center w-full h-4/5">
+        <CodeMirror
+          className="max-h-full w-1/2 h-full  pr-2"
+          value="console.log()"
+        />
+        <CodeMirror
+          className="w-1/2 h-full  pl-2"
+          value="console.log()"
+          basicSetup={{
+            lineNumbers: false,
+            foldGutter: false,
+          }}
+          readOnly={true}
+          editable={false}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GraphiQL;
