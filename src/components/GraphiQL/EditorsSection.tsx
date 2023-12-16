5d886e317ce8efa17f6021b28f2d0950df4c781b
Novik Dmytro <dinovik@rambler.ru>
2023-12-17 02:20:44 +0300

    feat: tweak editor height on wide screens to fill remaining height of the container

--- src/components/GraphiQL/EditorsSection.tsx
+++ src/components/GraphiQL/EditorsSection.tsx
@@ -5,7 +5,7 @@ import { editorTheme } from '../../themes/codemirror';
 const EditorsSection = () => {
   return (
     <div className="flex flex-col w-full md:max-h-full md:w-1/2 md:h-full px-[20px] my-2 md:my-0 border-2 md:mr-[5px]">
-      <div className="max-w-full overflow-auto">
+      <div className="max-w-full overflow-auto md:h-full">
         <CodeMirror
           theme={editorTheme}
           className="text-sm"
