c6af850c1276b34d83497a31b8c5937c6309ef67
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-12-19 23:20:50 +0300

    feat: add initial rtk query api hooks

---
+++ src/services/api.ts
@@ -0,0 +1,67 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { setEndpointState } from '../store/slices/endpointSlice';
+import { DOCUMENTATION_QUERY } from '../dto/constants';
+
+const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery(),
+  endpoints: (builder) => ({
+    getSchema: builder.query<string, { url: string }>({
+      query: ({ url }) => ({
+        url,
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          operationName: 'IntrospectionQuery',
+          variables: {},
+          query: DOCUMENTATION_QUERY,
+        }),
+      }),
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        dispatch(setEndpointState({ isLoading: true, isValid: false }));
+        try {
+          await queryFulfilled;
+          dispatch(setEndpointState({ isLoading: false, isValid: true }));
+        } catch (err) {
+          dispatch(setEndpointState({ isLoading: false, isValid: false }));
+        }
+      },
+    }),
+    getGraphQLData: builder.query<
+      string,
+      {
+        url: string;
+        operationName: string | null;
+        query: string;
+        variables: { [key: string]: string };
+      }
+    >({
+      query: ({ url, operationName, query, variables }) => ({
+        url,
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          operationName: operationName || null,
+          variables: variables || {},
+          query,
+        }),
+      }),
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        dispatch(setEndpointState({ isLoading: true, isValid: false }));
+        try {
+          await queryFulfilled;
+          dispatch(setEndpointState({ isLoading: false, isValid: true }));
+        } catch (err) {
+          dispatch(setEndpointState({ isLoading: false, isValid: false }));
+        }
+      },
+    }),
+  }),
+});
+
+export default api;
+export const { useGetSchemaQuery, useGetGraphQLDataQuery } = api;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './slices/userSlice';
 import authPathSlice from './slices/authPathSlice';
+import api from '../services/api';
+import endpointSlice from './slices/endpointSlice';
 
 export const store = configureStore({
   reducer: {
     [userSlice.name]: userSlice.reducer,
     [authPathSlice.name]: authPathSlice.reducer,
+    [endpointSlice.name]: endpointSlice.reducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
