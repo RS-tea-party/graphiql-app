2d812bd0964b686469be57dd5947cbb6a8efaf2d
IngaMuse <ingamoshkareva@gmail.com>
2023-12-26 00:16:35 +0300

    feat: add test signUp with mock

--- src/__test__/SignUp.test.tsx
+++ src/__test__/SignUp.test.tsx
@@ -4,6 +4,10 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import WrapperWithLocaleContext from './helpers/WrapperWithLocaleContext';
 import WrapperWithStore from './helpers/WrapperWithStore';
 import SignUp from '../components/Forms/SignUp';
+import MemoryRouterProvider from './helpers/MemoryRouterProvider';
+import { createUserWithEmailAndPassword } from '@firebase/auth';
+import { store } from '../store/store';
+import { regPath } from '../store/slices/authPathSlice';
 
 describe('SignUp component', () => {
   it('renders correctly', () => {
@@ -23,40 +27,112 @@ describe('SignUp component', () => {
     expect(screen.getByPlaceholderText('Имя')).toBeInTheDocument();
   });
 
-  describe('with invalid value', () => {
-    it('renders the email validation error', async () => {
-      render(
-        <WrapperWithStore>
-          <WrapperWithLocaleContext lang="ru">
-            <SignUp />
-          </WrapperWithLocaleContext>
-        </WrapperWithStore>
-      );
-      await waitFor(() => {
-        const emailInput = screen.getByPlaceholderText('name@mail.com');
-        fireEvent.change(emailInput, { target: { value: 'invalid email' } });
-        fireEvent.click(screen.getByRole('button', { name: 'Регистрация' }));
-      });
-      expect(
-        screen.getByText('Email не является допустимым')
-      ).toBeInTheDocument();
+  it('renders the name validation error', async () => {
+    render(
+      <WrapperWithStore>
+        <WrapperWithLocaleContext lang="ru">
+          <SignUp />
+        </WrapperWithLocaleContext>
+      </WrapperWithStore>
+    );
+    await waitFor(() => {
+      const nameInput = screen.getByPlaceholderText('Имя');
+      fireEvent.change(nameInput, { target: { value: '' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Регистрация' }));
     });
-    it('renders the password validation error', async () => {
-      render(
-        <WrapperWithStore>
-          <WrapperWithLocaleContext lang="ru">
-            <SignUp />
-          </WrapperWithLocaleContext>
-        </WrapperWithStore>
-      );
-      await waitFor(() => {
-        const emailInput = screen.getByPlaceholderText('********');
-        fireEvent.change(emailInput, { target: { value: '123q@' } });
-        fireEvent.click(screen.getByRole('button', { name: 'Регистрация' }));
-      });
-      expect(
-        screen.getByText('Должно быть минимум 8 символов')
-      ).toBeInTheDocument();
+    expect(
+      screen.getAllByText('Это поле обязательно к заполнению')[0]
+    ).toBeInTheDocument();
+  });
+  it('renders the email validation error', async () => {
+    render(
+      <WrapperWithStore>
+        <WrapperWithLocaleContext lang="ru">
+          <SignUp />
+        </WrapperWithLocaleContext>
+      </WrapperWithStore>
+    );
+    await waitFor(() => {
+      const emailInput = screen.getByPlaceholderText('name@mail.com');
+      fireEvent.change(emailInput, { target: { value: 'invalid email' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Регистрация' }));
     });
+    expect(
+      screen.getByText('Email не является допустимым')
+    ).toBeInTheDocument();
+  });
+  it('renders the password validation error', async () => {
+    render(
+      <WrapperWithStore>
+        <WrapperWithLocaleContext lang="ru">
+          <SignUp />
+        </WrapperWithLocaleContext>
+      </WrapperWithStore>
+    );
+    await waitFor(() => {
+      const passwordInput = screen.getByPlaceholderText('********');
+      fireEvent.change(passwordInput, { target: { value: '123q@' } });
+      fireEvent.click(screen.getByRole('button', { name: 'Регистрация' }));
+    });
+    expect(
+      screen.getByText('Должно быть минимум 8 символов')
+    ).toBeInTheDocument();
+  });
+
+  const userCredentialMock = {
+    user: {
+      sendEmailVerification: vi.fn(),
+    },
+  };
+  //const mockGetAuth = vi.fn()
+
+  vi.mock('firebase/auth', () => {
+    //     const firebasemock = require('firebase')
+    //     const firebase = firebasemock.initializeApp()
+    // const firebaseAuth = firebase.auth()
+    return {
+      //auth: vi.fn().mockReturnThis(),
+      createUserWithEmailAndPassword: vi.fn(() => userCredentialMock),
+      getAuth: () => vi.fn(),
+      // firebase,
+      // firebasemock
+    };
+  });
+
+  afterAll(() => {
+    vi.resetAllMocks();
+  });
+
+  it('should pass sign up', async () => {
+    store.dispatch(regPath());
+    render(
+      <WrapperWithStore>
+        <WrapperWithLocaleContext lang="ru">
+          <MemoryRouterProvider initialEntries={['/auth']} />
+        </WrapperWithLocaleContext>
+      </WrapperWithStore>
+    );
+
+    //screen.debug();
+    const email = 'name@mail.com';
+    const password = '123!@#qwe';
+    await waitFor(() => {
+      const nameInput = screen.getByPlaceholderText('Имя');
+      fireEvent.change(nameInput, { target: { value: 'RS' } });
+      const emailInput = screen.getByPlaceholderText('name@mail.com');
+      fireEvent.change(emailInput, { target: { value: email } });
+      const passwordInput = screen.getByPlaceholderText('********');
+      fireEvent.change(passwordInput, { target: { value: password } });
+      fireEvent.click(
+        screen.getAllByRole('button', { name: 'Регистрация' })[1]
+      );
+    });
+
+    expect(createUserWithEmailAndPassword).toBeCalledWith(
+      undefined,
+      email,
+      password
+    );
+    //expect(userCredentialMock.user?.sendEmailVerification).toBeCalled();
   });
 });
