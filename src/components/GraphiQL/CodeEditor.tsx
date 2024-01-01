a3c9d96ad5f1cc4e6890175a386e0caeeffd9efa
Novik Dmytro <dinovik@rambler.ru>
2024-01-01 18:06:30 +0300

    feat: modify main editor to store it's value in state

--- src/components/GraphiQL/CodeEditor.tsx
+++ src/components/GraphiQL/CodeEditor.tsx
@@ -1,12 +1,13 @@
 import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { editorTheme, resultsTheme } from '../../themes/codemirror';
-import { PropsWithChildren } from 'react';
+import { Dispatch, PropsWithChildren, SetStateAction } from 'react';
 import { forwardRef } from 'react';
 import { EditorView } from '@uiw/react-codemirror';
 
 interface CodeEditorProps {
   mode: 'editor' | 'viewer';
   defaultValue?: string;
+  onchange?: Dispatch<SetStateAction<string>>;
 }
 
 const CodeEditor = forwardRef<
@@ -27,6 +28,7 @@ const CodeEditor = forwardRef<
         value={props.defaultValue}
         theme={props.mode === 'editor' ? editorTheme : resultsTheme}
         className="text-sm"
+        onChange={props.onchange}
         basicSetup={{
           lineNumbers: props.mode === 'editor',
           foldGutter: props.mode === 'editor',
