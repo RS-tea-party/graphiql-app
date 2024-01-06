206f0ec7839a4b5abb15eff0ea81fbef3eec5823
Novik Dmytro <dinovik@rambler.ru>
2024-01-07 00:20:40 +0300

    feat: add lazyLoading docs from the api upon opening

--- src/components/GraphiQL/DocsModal.tsx
+++ src/components/GraphiQL/DocsModal.tsx
@@ -1,42 +1,70 @@
-import { FC, useContext } from 'react';
+import { FC, Suspense, useContext, useEffect } from 'react';
 import { useAppSelector } from '../../hooks/useAppSelector';
-import { closeDocs, docsSelector } from '../../store/slices/docsSlice';
+import {
+  closeDocs,
+  docsShown as docsShownSelector,
+} from '../../store/slices/docsSlice';
 import { prettify } from '../../helpers/prettify';
 import CodeEditor from './CodeEditor';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import ButtonThemed from '../_ui/ButtonThemed/ButtonThemed';
 import { LocaleContext } from '../LocaleContext/LocaleContext';
+import Loader from '../Loader/Loader';
+import { urlSelector } from '../../store/slices/endpointSlice';
+import { useLazyGetSchemaQuery } from '../../services/api';
+import { Typography } from '@material-tailwind/react';
 
 const DocsModal: FC = () => {
-  const docs = useAppSelector(docsSelector);
+  const docsShown = useAppSelector(docsShownSelector);
+  const url = useAppSelector(urlSelector);
+  const [trigger, result] = useLazyGetSchemaQuery();
+  const { data, isSuccess, isError } = result;
+
   const dispatch = useAppDispatch();
   const { spellingList } = useContext(LocaleContext);
   const genericHamburgerLine = `h-0.5 w-5 my-1 rounded-full bg-blue-gray-900 transition ease transform duration-300`;
-
   const handleCloseDocs = () => {
     dispatch(closeDocs());
   };
 
+  useEffect(() => {
+    if (docsShown) trigger({ url });
+  }, [docsShown, trigger, url]);
+
   return (
-    <div className="flex border-4 border-white rounded-lg absolute z-20 w-[85%] max-h-[calc(100vh-169.6px)]">
-      <CodeEditor mode="docs" defaultValue={prettify(docs)}>
-        <ButtonThemed
-          className="opacity-50 rounded-full p-2 hover:bg-peachFuzz"
-          variant="outlined"
-          tooltip={{ text: spellingList.graphiQL.close, position: 'left' }}
-          onClick={handleCloseDocs}
-          zindex="z-20"
-        >
-          <div
-            className={`${genericHamburgerLine} ${'rotate-45 translate-y-1.5 group-hover:opacity-100'}`}
-          />
-          <div className={`${genericHamburgerLine} ${'opacity-0'}`} />
-          <div
-            className={`${genericHamburgerLine} ${'-rotate-45 -translate-y-1.5 group-hover:opacity-100'}`}
-          />
-        </ButtonThemed>
-      </CodeEditor>
-    </div>
+    <section className="flex border-4 border-white rounded-lg absolute z-20 w-[85%] max-h-[calc(100vh-169.6px)] min-h-[50px] bg-white">
+      <Suspense fallback={<Loader />}>
+        {isSuccess ? (
+          <CodeEditor mode="docs" defaultValue={prettify(JSON.stringify(data))}>
+            <ButtonThemed
+              className="opacity-50 rounded-full p-2 hover:bg-peachFuzz"
+              variant="outlined"
+              tooltip={{ text: spellingList.graphiQL.close, position: 'left' }}
+              onClick={handleCloseDocs}
+              zindex="z-20"
+            >
+              <div
+                className={`${genericHamburgerLine} ${'rotate-45 translate-y-1.5 group-hover:opacity-100'}`}
+              />
+              <div className={`${genericHamburgerLine} ${'opacity-0'}`} />
+              <div
+                className={`${genericHamburgerLine} ${'-rotate-45 -translate-y-1.5 group-hover:opacity-100'}`}
+              />
+            </ButtonThemed>
+          </CodeEditor>
+        ) : isError ? (
+          <Typography
+            variant="h4"
+            color="blue-gray"
+            className="mb-2 w-full text-center"
+          >
+            Nothing found
+          </Typography>
+        ) : (
+          <Loader />
+        )}
+      </Suspense>
+    </section>
   );
 };
 
